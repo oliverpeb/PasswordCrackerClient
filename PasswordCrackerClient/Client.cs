5b4d82fdf044c99042b8d21316226dc0a98e4355
oliverpeb <83925702+oliverpeb@users.noreply.github.com>
2023-09-18 14:34:05 +0200

    Kan sende filer.

--- PasswordCrackerClient/Client.cs
+++ PasswordCrackerClient/Client.cs
@@ -4,25 +4,25 @@ using System.Text;
 
 public class Client
 {
-    private const string ServerAddress = "127.0.0.1";
+    private const string ServerAddress = "172.20.10.13";
     private const int ServerPort = 12345;
 
-    public void SendMessage(string message)
+    public void SendFile(string filePath)
     {
         using (TcpClient client = new TcpClient(ServerAddress, ServerPort))
         using (NetworkStream stream = client.GetStream())
         {
-            byte[] data = Encoding.ASCII.GetBytes(message);
+            byte[] data = System.IO.File.ReadAllBytes(filePath);
             stream.Write(data, 0, data.Length);
-            Console.WriteLine($"Sent: {message}");
+            Console.WriteLine($"Sent file: {filePath}");
         }
     }
 
     public static void Main()
     {
         Client client = new Client();
-        Console.WriteLine("Enter a message to send to the server:");
-        string message = Console.ReadLine();
-        client.SendMessage(message);
+        Console.WriteLine("Enter the path of the text file to send to the server:");
+        string filePath = Console.ReadLine();
+        client.SendFile(filePath);
     }
 }
